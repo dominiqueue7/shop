28e61b498b1217c6a768bad4f268cd0f495aaec2
dominiqueue7 <dominiqueue7@gmail.com>
2023-02-10 00:34:52 +0900

    second

---
+++ src/data.js
@@ -0,0 +1,26 @@
+let data = [
+    {
+        id: 0,
+        title: "Playstation 5",
+        content: "Play Has No Limits",
+        price: 120000,
+        pic: "./img/ps5.jpg"
+    },
+
+    {
+        id: 1,
+        title: "Bluetooth Speaker",
+        content: "Powerful sound",
+        price: 110000,
+        pic: "./img/speaker.jpg"
+    },
+
+    {
+        id: 2,
+        title: "Rice cooker",
+        content: "IH Vacuum pressure",
+        price: 130000,
+        pic: "./img/cooker.jpg"
+    }
+]
+export default data
